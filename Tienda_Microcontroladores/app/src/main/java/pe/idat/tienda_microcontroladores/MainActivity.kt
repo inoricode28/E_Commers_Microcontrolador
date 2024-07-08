ebb5baf6238d9bde990e4f6ce48d8c8140bd2019
inoricode28 <inori237@gmail.com>
2024-07-08 01:07:41 -0500

    Base

---
+++ Tienda_Microcontroladores/app/src/main/java/pe/idat/tienda_microcontroladores/MainActivity.kt
@@ -0,0 +1,36 @@
+package pe.idat.tienda_microcontroladores
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import pe.idat.tienda_microcontroladores.ui.theme.Tienda_MicrocontroladoresTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            Tienda_MicrocontroladoresTheme {
+
+            }
+        }
+    }
+}
+
+
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    Tienda_MicrocontroladoresTheme {
+
+    }
+}
