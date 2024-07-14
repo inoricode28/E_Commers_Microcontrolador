820770ee48d17969ada305493af526e10c138823
inoricode28 <inori237@gmail.com>
2024-07-14 15:43:40 -0500

    Loginv1

--- Tienda_Microcontroladores/app/src/main/java/pe/idat/tienda_microcontroladores/LoginScreen.kt
+++ Tienda_Microcontroladores/app/src/main/java/pe/idat/tienda_microcontroladores/LoginScreen.kt
@@ -2,24 +2,36 @@ package pe.idat.tienda_microcontroladores
 
 import android.app.Activity
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Close
+import androidx.compose.material.icons.filled.Visibility
+import androidx.compose.material.icons.filled.VisibilityOff
 import androidx.compose.material3.Button
+import androidx.compose.material3.Divider
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
+import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -27,6 +39,7 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 
@@ -35,24 +48,59 @@ fun LoginScreen() {
     Box(
         Modifier
             .fillMaxSize()
-            .padding(8.dp)) {
+            .padding(8.dp)
+    ) {
         Header(Modifier.align(Alignment.TopEnd))
         Body(Modifier.align(Alignment.Center))
+        Footer(Modifier.align(Alignment.BottomCenter))
     }
 
 }
 
 @Composable
-fun Body(modifier: Modifier){
-    var email by rememberSaveable{mutableStateOf("")}
-    var password by rememberSaveable{mutableStateOf("")}
-    var isLoginEnable by rememberSaveable{mutableStateOf(false)}
+fun Footer(modifier: Modifier) {
+    Column(modifier = modifier.fillMaxWidth()) {
+        Divider(
+            Modifier
+                .background(Color(0xFFF9F9F9))
+                .height(1.dp)
+                .fillMaxWidth()
+        )
+        Spacer(modifier = Modifier.size(24.dp))
+        SingUp()
+        Spacer(modifier = Modifier.size(32.dp))
+
+    }
+
+
+}
+
+@Composable
+fun SingUp() {
+    Row(Modifier.fillMaxWidth(), horizontalArrangement = Arrangement.Center) {
+        Text(text = "Don't have an account?", fontSize = 12.sp, color = Color(0xFFB5B5B5))
+        Text(
+            text = "Sing up.",
+            Modifier.padding(horizontal = 8.dp),
+            fontSize = 12.sp,
+            fontWeight = FontWeight.Bold,
+            color = Color(0xFF4EA8E9)
+        )
+
+    }
+}
+
+@Composable
+fun Body(modifier: Modifier) {
+    var email by rememberSaveable { mutableStateOf("") }
+    var password by rememberSaveable { mutableStateOf("") }
+    var isLoginEnable by rememberSaveable { mutableStateOf(false) }
     Column(modifier = modifier) {
         ImageLogo(Modifier.align(Alignment.CenterHorizontally))
         Spacer(modifier = Modifier.size(16.dp))
-        Email(email) {email = it}
+        Email(email) { email = it }
         Spacer(modifier = Modifier.size(8.dp))
-        Password(password){password = it}
+        Password(password) { password = it }
         Spacer(modifier = Modifier.size(8.dp))
         ForgotPassword(Modifier.align(Alignment.End))
         Spacer(modifier = Modifier.size(16.dp))
@@ -64,9 +112,9 @@ fun Body(modifier: Modifier){
 
 @Composable
 fun LoginButton(loginEnable: Boolean) {
-    Button(onClick = {  }, enabled = loginEnable, modifier = Modifier.fillMaxWidth()) {
+    Button(onClick = { }, enabled = loginEnable, modifier = Modifier.fillMaxWidth()) {
         Text(text = "Log In")
-        
+
     }
 
 
@@ -74,26 +122,87 @@ fun LoginButton(loginEnable: Boolean) {
 
 @Composable
 fun ForgotPassword(modifier: Modifier) {
-    Text(text = "Forgot password?", fontSize = 12.sp, fontWeight = FontWeight.Bold, color = Color(0xFF4EA8E9),modifier = modifier)
+    Text(
+        text = "Forgot password?",
+        fontSize = 12.sp,
+        fontWeight = FontWeight.Bold,
+        color = Color(0xFF4EA8E9),
+        modifier = modifier
+    )
 }
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun Password(password:String, onTextChanged:(String) -> Unit) {
-    TextField(value = password, onValueChange = {onTextChanged(it)}, modifier = Modifier.fillMaxWidth())
+fun Password(password: String, onTextChanged: (String) -> Unit) {
+    var passwordVisibility by remember { mutableStateOf(false) }
+    TextField(
+        value = password,
+        onValueChange = { onTextChanged(it) },
+        modifier = Modifier.fillMaxWidth(),
+        placeholder = { Text(text = "Password")},
+        colors = TextFieldDefaults.textFieldColors(
+            containerColor = Color(0xFFFAFAFA),
+            unfocusedIndicatorColor = Color.Transparent,
+            focusedIndicatorColor = Color.Transparent
+
+        )
+        ,
+        singleLine = true,
+        maxLines = 1,
+        keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Password),
+        trailingIcon = {
+            val imagen = if(passwordVisibility){
+                Icons.Filled.VisibilityOff
+
+            }else{
+                Icons.Filled.Visibility
+
+            }
+            IconButton(onClick = { passwordVisibility = !passwordVisibility }) {
+                Icon(imageVector = imagen, contentDescription = "Show Password")
+                
+            }
+
+        }
+    )
 }
+
+
+
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun Email(email:String, onTextChanged:(String) -> Unit) {
-    TextField(value = email, onValueChange = {onTextChanged(it)}, modifier = Modifier.fillMaxWidth())
+fun Email(email: String, onTextChanged: (String) -> Unit) {
+    TextField(
+        value = email,
+        onValueChange = { onTextChanged(it) },
+        modifier = Modifier.fillMaxWidth(),
+        placeholder = { Text(text = "Email")},
+        maxLines = 1,
+        singleLine = true,
+        keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Email),
+        colors = TextFieldDefaults.textFieldColors(
+           // textColor = Color(0xFF2B2B2B),
+            containerColor = Color(0xFFFAFAFA),
+            unfocusedIndicatorColor = Color.Transparent,
+            focusedIndicatorColor = Color.Transparent
+
+        )
+
+
+    )
 }
 
 @Composable
 fun ImageLogo(modifier: Modifier) {
-    Image(painter = painterResource(id = R.drawable.insta), contentDescription = "Logo", modifier = modifier)
-
+    Image(
+        painter = painterResource(id = R.drawable.insta),
+        contentDescription = "Logo",
+        modifier = modifier
+    )
 }
 
 @Composable
-fun Header(modifier: Modifier){
+fun Header(modifier: Modifier) {
     val activity = LocalContext.current as Activity
     Icon(imageVector = Icons.Default.Close, contentDescription = "close app",
         modifier = modifier.clickable { activity.finish() })
